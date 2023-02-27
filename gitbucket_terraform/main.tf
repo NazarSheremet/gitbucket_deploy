857d9c3d7d8f6143f0e767e5cef3ca0fbe258809
“NazarSheremet” <nazarsheremet03@gmail.com>
2023-02-27 02:53:47 +0200

    add var

--- gitbucket_terraform/main.tf
+++ gitbucket_terraform/main.tf
@@ -1,5 +1,3 @@
-# We strongly recommend using the required_providers block to set the
-# Azure Provider source and version being used
 terraform {
   required_providers {
     azurerm = {
@@ -9,22 +7,22 @@ terraform {
   }
 }
 
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
 }
 
+
 # Create a resource group
-resource "azurerm_resource_group" "gitbucket2" {
-  name     = "gitbucket2"
+resource "azurerm_resource_group" "gitbucket" {
+  name     = "gitbucket"
   location = "West Europe"
 
 }
 
-resource "azurerm_postgresql_server" "gitbucket2" {
-  name                = "postgresql-gitbucket54543543"
-  location            = azurerm_resource_group.gitbucket2.location
-  resource_group_name = azurerm_resource_group.gitbucket2.name
+resource "azurerm_postgresql_server" "gitbucket" {
+  name                = "postgresql-gitbucket"
+  location            = azurerm_resource_group.gitbucket.location
+  resource_group_name = azurerm_resource_group.gitbucket.name
 
   sku_name = "B_Gen5_2"
 
@@ -33,80 +31,56 @@ resource "azurerm_postgresql_server" "gitbucket2" {
   geo_redundant_backup_enabled = false
   auto_grow_enabled            = true
 
-  administrator_login          = "psqladmin"
-  administrator_login_password = "P@ssw0rd"
+  administrator_login          = var.db_user
+  administrator_login_password = var.db_password
   version                      = "11"
   ssl_enforcement_enabled      = true
 }
 
 # Create a database
-resource "azurerm_postgresql_database" "gitbucket2" {
+resource "azurerm_postgresql_database" "gitbucket" {
   name                = "gitbucket"
-  resource_group_name = azurerm_resource_group.gitbucket2.name
-  server_name         = azurerm_postgresql_server.gitbucket2.name
+  resource_group_name = azurerm_resource_group.gitbucket.name
+  server_name         = azurerm_postgresql_server.gitbucket.name
   charset             = "UTF8"
   collation           = "English_United States.1252"
 }
 
-resource "azurerm_postgresql_firewall_rule" "gitbucket2" {
-  name                = "gitbucket"
-  resource_group_name = azurerm_resource_group.gitbucket2.name
-  server_name         = azurerm_postgresql_server.gitbucket2.name
-  start_ip_address    = "0.0.0.0"
-  end_ip_address      = "255.255.255.255"
+resource "azurerm_postgresql_firewall_rule" "gitbucket" {
+  count               = length(var.outbound_ip_address_list)
+  name                = count.index
+  resource_group_name = azurerm_resource_group.gitbucket.name
+  server_name         = azurerm_postgresql_server.gitbucket.name
+  start_ip_address    = element(var.outbound_ip_address_list, count.index)
+  end_ip_address      = element(var.outbound_ip_address_list, count.index)
 }
 
-resource "azurerm_service_plan" "gitbucket2" {
-  name                = "gitbucket2-appserviceplan"
-  location            = azurerm_resource_group.gitbucket2.location
-  resource_group_name = azurerm_resource_group.gitbucket2.name
+
+resource "azurerm_service_plan" "gitbucket" {
+  name                = "gitbucket-appserviceplan"
+  location            = azurerm_resource_group.gitbucket.location
+  resource_group_name = azurerm_resource_group.gitbucket.name
   os_type             = "Linux"
   sku_name            = "B1"
 }
 
 
-resource "azurerm_linux_web_app" "gitbucket2" {
-  name                = "gitbucket2"
-  location            = azurerm_resource_group.gitbucket2.location
-  resource_group_name = azurerm_resource_group.gitbucket2.name
-  service_plan_id     = azurerm_service_plan.gitbucket2.id
-
+resource "azurerm_linux_web_app" "gitbucket" {
+  name                = "gitbucket"
+  location            = azurerm_resource_group.gitbucket.location
+  resource_group_name = azurerm_resource_group.gitbucket.name
+  service_plan_id     = azurerm_service_plan.gitbucket.id
 
   site_config {
     application_stack {
       java_server  = "TOMCAT"
-      java_version = "8"
+      java_version = "11"
     }
   }
   app_settings = {
-    "GITBUCKET_DB_URL" = "jdbc:postgresql://postgresql-gitbucket54543543.postgres.database.azure.com:5432/gitbucket"
-    "GITBUCKET_DB_USER" = "psqladmin@postgresql-gitbucket54543543"
-    "GITBUCKET_DB_PASSWORD" = "P@ssw0rd"
+    "GITBUCKET_DB_URL"      = var.db_url
+    "GITBUCKET_DB_USER"     = var.db_user
+    "GITBUCKET_DB_PASSWORD" = var.db_password
   }
 
-}
-
-resource "azurerm_postgresql_server" "gitbucket3" {
-  name                = "postgresql-gitbucket5454354"
-  location            = azurerm_resource_group.gitbucket2.location
-  resource_group_name = azurerm_resource_group.gitbucket2.name
-
-  sku_name = "B_Gen5_2"
-
-  storage_mb                   = 5120
-  backup_retention_days        = 7
-  geo_redundant_backup_enabled = false
-  auto_grow_enabled            = true
-
-  administrator_login          = "psqladmin"
-  administrator_login_password = "P@ssw0rd"
-  version                      = "11"
-  ssl_enforcement_enabled      = true
-}
-resource "azurerm_postgresql_firewall_rule" "gitbucket3" {
-  name                = "gitbucket"
-  resource_group_name = azurerm_resource_group.gitbucket2.name
-  server_name         = azurerm_postgresql_server.gitbucket2.name
-  start_ip_address    = "0.0.0.0"
-  end_ip_address      = "255.255.255.255"
 }
---
+++ gitbucket_terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "outbound_ip_address_list" {
+  description = "ip address of app service"
+  default = [
+    ###
+  ]
+
+}
+
+variable "db_url" {
+  default = "###"
+}
+variable "db_user" {
+  default = "###"
+}
+variable "db_password" {
+  default = "###"
+}
